d69a08510d3d5565368e00920a03a5bc33cb0190
RohitG28 <rohit.ghivdonde@gmail.com>
2017-07-07 12:36:56 +0530

    minor changes

--- scatterPlot.R
+++ scatterPlot.R
@@ -17,14 +17,16 @@ scatterPlot <- function(jsonString)
   library("jsonlite")
 
   data.json <- jsonlite::fromJSON(jsonString,simplifyVector = TRUE)
-  rgl::plot3d(x=data.json$nodes$x,y=data.json$nodes$y,z=data.json$nodes$z,xlab="x",ylab="y",zlab="z",size=10,col="blue",box=FALSE)
-  rgl::planes3d(a=0,b=0,c=1,d=(-1)*data.json$nodes$z,alpha=0.5)
+  data.nodes=as.data.frame(data.json$nodes)
+  data.links=as.data.frame(data.json$links)
+  rgl::plot3d(x=data.nodes$x,y=data.nodes$y,z=data.nodes$z,xlab="x",ylab="y",zlab="z",size=10,col="blue",box=FALSE)
+  rgl::planes3d(a=0,b=0,c=1,d=(-1)*data.nodes$z,alpha=0.5)
 
   count=1
-  while(count<=nrow(data.json$links$source)){
-    rgl::lines3d(x=c(data.json$links$source$x[c(count)],data.json$links$target$x[c(count)]),
-                 y=c(data.json$links$source$y[c(count)],data.json$links$target$y[c(count)]),
-                 z=c(data.json$links$source$z[c(count)],data.json$links$target$z[c(count)]),
+  while(count<=nrow(data.links$source)){
+    rgl::lines3d(x=c(data.links$source$x[c(count)],data.links$target$x[c(count)]),
+                 y=c(data.links$source$y[c(count)],data.links$target$y[c(count)]),
+                 z=c(data.links$source$z[c(count)],data.links$target$z[c(count)]),
                  add=TRUE)
     count=count+1
     }
